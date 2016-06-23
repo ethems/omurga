39b1a251e0af6394f0d6c7f74b1c29ad333b915f
furkano <Furkan.Ozdeslik@confirmit.com>
2016-06-22 21:37:12 -0700

    create omurga

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+      'script!jquery/dist/jquery.min.js',
+      'script!materialize-css/dist/js/materialize.min.js',
+      './app',
+    ],
+    externals:{
+      jquery:'jQuery'
+    },
+    plugins:[
+      new webpack.ProvidePlugin({
+        '$':'jquery',
+        'jQuery':'jquery'
+      })
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        root:__dirname,
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            },
+            {test: /\.(jpg|png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,loader: 'url?limit=100000'}
+        ]
+    }
+};
